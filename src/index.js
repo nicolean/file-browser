f50dc0ffc2798e93a45f6ac01ca850130f1c49e6
Nicole <neanwebdev@gmail.com>
2019-03-19 02:16:53 -0700

    working on styling the file-line correctly

--- src/index.js
+++ src/index.js
@@ -26,41 +26,89 @@ const getSiblings = el => {
   })
   return siblings;
 }
+const getChildByClass = (el, className) => {
+  const childrenList = getChildren(el);
+  let child;
+  childrenList.map( val => {
+    if (hasClass(val, className)) {
+      child = val;
+    }
+  })
+  return child; 
+}
+const getChildren = el => {
+  let children = el.children;
+  let childrenList = [];
+  Array.prototype.filter.call(children, val => {
+    childrenList.push(val);
+  })
+  return childrenList;
+}
 
-const _init = ({ container = '', data = [] }) => {
+const resetOpenStatus = folderIconEl => {
+  folderIconEl.classList.remove('folder-open');
+  getSiblingByClass(folderIconEl.parentNode, 'dir-file-container').classList.add('hide');
+}
+
+const _init = ({ container = '', data = [], onClick = false }) => {
   let fileBrowserContainer = document.getElementById(container);
-  fileBrowserContainer.innerHTML = parseData(data);
+  fileBrowserContainer.innerHTML = parseData(data, onClick);
 
-  console.log(document.getElementsByClassName('.file-name'));
+  let fileLineDivHeight = document.getElementsByClassName('file-name')[0].clientHeight / 4;
+  Array.from(document.getElementsByClassName('file-line')).forEach( div => {
+    div.style.height = fileLineDivHeight+'px';
+  })
+
   document.addEventListener('click', e => {
     let el = e.target;
     if (hasClass(el, 'file-name')) {
-      console.log('file');
       toggleClass(el, 'green');
-    } else if (hasClass(el, 'dir-name')) {
-      toggleClass(getSiblingByClass(el, 'icon'), 'folder-open'); // TODO: add check here to toggle only if !hide&folder-open or hide&!folder-open
-      toggleClass(getSiblingByClass(el.parentNode, 'dir-file-container'), 'hide');
-    } 
+    } else if (hasClass(el, 'dir-name-container')) {
+      let folderIcon = getChildByClass(el, 'icon');
+      let dirFileContainer = getSiblingByClass(el, 'dir-file-container');
+      if (hasClass(folderIcon, 'folder-open') !== hasClass(dirFileContainer, 'hide')) {
+        toggleClass(folderIcon, 'folder-open');
+        toggleClass(dirFileContainer, 'hide');
+      } else {
+        console.log('class and hide status mismatch, resetting');
+        resetOpenStatus(folderIcon);
+      }
+    } else if (hasClass(el, 'dir-name') || hasClass(el, 'dir-icon')) {
+      let folderIcon = getSiblingByClass(el, 'icon');
+      let dirFileContainer = getSiblingByClass(el.parentNode, 'dir-file-container');
+      if (hasClass(folderIcon, 'folder-open') !== hasClass(dirFileContainer, 'hide')) {
+        toggleClass(folderIcon, 'folder-open');
+        toggleClass(dirFileContainer, 'hide');
+      } else {
+        console.log('class and hide status mismatch, resetting');
+        resetOpenStatus(folderIcon);
+      }
+    }
   })
 }
 
-const parseData = fileData => {
+const parseData = (fileData, onClick) => {
+  // console.log(onClick);
   let folderDivs = '';
   fileData.map(dir => {
     let fileDivs = '';
-    dir.files.map(file => {
+    dir.files.map((file, i) => {
       fileDivs += `<div class="file-container">
-      <div class="file-name">${file}</div>
+        <div class="file-inner-container">
+          <div class="file-line"></div>
+          <div class="file-name" onClick="${onClick}()">${file}</div>
+        </div>
       </div>`;
     })
-
     folderDivs += 
     `<div class="dir-container">
       <div class="dir-name-container">
-        <span class="icon folder"></span>
+        <span class="icon folder dir-icon"></span>
         <div class="dir-name">${dir.name}</div>
       </div>
-      <div class="dir-file-container hide">${fileDivs}</div>
+      <div class="dir-file-container">
+        ${fileDivs}
+      </div>
     </div>`;
   })
   return folderDivs;
